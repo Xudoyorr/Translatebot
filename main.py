2dcbfd709310633648eca53edbfe169d8e59fb00
Xudoyor_Coder <95615223+Xudoyorr@users.noreply.github.com>
2022-01-17 13:34:58 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,67 @@
-from buttons import menu,orqa
-from aiogram.dispatcher.filters import Text
-
 import logging
-
-from config import TOKEN
 from aiogram import Bot, Dispatcher, executor, types
-
-
-API_TOKEN = TOKEN
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
+from config import TOKEN
+from button import menu
+from state import Translate
+from googletrans import Translator
+from aiogram.dispatcher.filters import Text
 
 logging.basicConfig(level=logging.INFO)
 
-bot = Bot(API_TOKEN)
-dp = Dispatcher(bot)
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot, storage=MemoryStorage())
 
-@dp.message_handler(commands=['start'])
-async def boshlash(message: types.Message):
-    """Botga /start buyrug'i yozilganda ishga tushadi"""
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
     user = message.from_user.first_name
-    await message.reply(f"Salom 😊{user}",reply_markup=menu)
-    await message.reply("Matnlarni tarjima qilish uchun tugmalardan birini tanlang✅")
+    await message.answer(f"Assalomu aleykum {user}😊\nTarjima turini tanlang", reply_markup=menu)
+    await Translate.lang.set()
 
 @dp.message_handler()
 async def echo(message: types.Message):
     id = message.chat.id
 @dp.message_handler(Text(equals="Tarjima qilmoqchi bolgan matnni kiriting😊"))
 async def echo(message: types.Message):
-    await message.answer('matn kiriting',reply_markup=orqa)
+    await message.answer('matn kiriting',reply_markup=menu)
+
+@dp.message_handler(state=Translate.lang)
+async def which_lang(message: types.Message, state: FSMContext):
+    lang = message.text
+    await state.update_data(
+        {"lang": lang},
+    )
+    await message.answer(f"Tarjima qilinuvchi matnni kiriting")
+    await Translate.next()
+    # await Translate.trans.set()
+
+@dp.message_handler(state=Translate.trans)
+async def translate_text(message: types.Message, state: FSMContext):
+    text = message.text
+    data = await state.get_data()
+    lang = data.get("lang")
+    tarjimon = Translator()
+    if lang == "🇺🇿 Uzb - 🇬🇧 Eng":
+        tarjima = tarjimon.translate(text, dest="en")
+        await message.answer(tarjima.text, reply_markup=menu)
+    elif lang == "🇬🇧 Eng - 🇺🇿 Uzb":
+        tarjima = tarjimon.translate(text, dest="uz")
+        await message.answer(tarjima.text, reply_markup=menu)
+    elif lang == "🇺🇿 Uzb - 🇷🇺 Rus":
+        tarjima = tarjimon.translate(text, dest="ru")
+        await message.answer(tarjima.text, reply_markup=menu)
+    elif lang == "🇷🇺 Rus - 🇺🇿 Uzb":
+        tarjima = tarjimon.translate(text, dest="uz")
+        await message.answer(tarjima.text, reply_markup=menu)
+    await Translate.lang.set()
+    # await state.finish()
+
+
+    
 
 @dp.message_handler(Text(equals='Orqaga🔙'))
 async def sshfj(message):
     await menu.answer('Menu',reply_markup=menu)
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)  
+    executor.start_polling(dp, skip_updates=True)
